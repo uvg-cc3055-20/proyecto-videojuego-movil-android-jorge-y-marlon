e95e6a4fd1e5d7d7a8f1103aae50c94134091480
marlord <marlon_dx@hotmail.com>
2018-02-11 16:56:16 -0600

    Escenas generales
    Se tiene la mayoria de escenas pero faltan algunas funcionabilidades. La escena 1 esta terminada. La escena 2 necesita un arreglo de los fonts para que se vea como se tiene que ver. Las escenas tres y cuatro faltan la jugabilidad

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour {
+    private AudioSource music;
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().Equals("CupheadGame"))
+        {
+            Destroy(music);
+        }
+        if (SceneManager.GetActiveScene().Equals("AngelGame"))
+        {
+            Destroy(music);
+        }
+        else
+        {
+            DontDestroyOnLoad(music);
+        }
+        
+
+    }
+}
