a360e0e297bd2c8d475560b14a54c89fa6e75fb7
marlord <marlon_dx@hotmail.com>
2018-02-12 17:06:19 -0600

    Menu, pausa, animaciones, y correr
    plz push

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    Animator anim;
+
+    Rigidbody2D rb;
+    float speed = 10f;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (PauseMenu.instance.GameState == false)
+        {
+            float movX = Input.acceleration.x;
+            rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
+            anim.SetFloat("Speed", Mathf.Abs(movX));
+        }
+        
+    }
+}
